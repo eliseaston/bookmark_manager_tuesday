37d0d645de799f2134ab192ce1903b3e486394f6
Sara <srancati0@gmail.com>
2019-11-04 17:27:40 +0000

    end of step 6. Manipulating Table Data

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -3,7 +3,8 @@ class Bookmark
   def self.all
     [
     "http://www.makersacademy.com",
-    "http://www.google.com"
+    "http://www.google.com",
+    "http://www.destroyallsoftware.com"
     ]
   end
 
