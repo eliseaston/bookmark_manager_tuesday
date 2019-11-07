601a4e46ee92e5a6ad55e7094af9701de1a2862a
ellieSMASH <eliseaston@gmail.com>
2019-11-07 14:53:43 +0000

    refactor tests to use new .create method

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -3,17 +3,13 @@ require 'bookmark'
 describe Bookmark do
   describe '#all' do
     it 'returns all bookmarks' do
-      connection = PG.connect(dbname: 'bookmark_manager_test')
+      Bookmark.create('http://www.makersacademy.com')
+      Bookmark.create('http://www.destroyallsoftware.com')
+      Bookmark.create('http://www.google.com')
 
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com')")
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com')")
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.google.com')")
-
-      bookmarks = Bookmark.all
-
-      expect(bookmarks).to include("http://www.makersacademy.com")
-      expect(bookmarks).to include("http://www.destroyallsoftware.com")
-      expect(bookmarks).to include("http://www.google.com")
+      expect(Bookmark.all).to include("http://www.makersacademy.com")
+      expect(Bookmark.all).to include("http://www.destroyallsoftware.com")
+      expect(Bookmark.all).to include("http://www.google.com")
     end
   end
 
